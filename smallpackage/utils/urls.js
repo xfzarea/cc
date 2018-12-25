b5e5d1c39bbc3b9a2a5fd47d936196fb1724356a
由于木 <>
2018-12-25 16:55:17 +0800

    修改包开口提现问题

--- smallpackage/utils/urls.js
+++ smallpackage/utils/urls.js
@@ -1,6 +1,6 @@
 module.exports = {
-  profit:"https://www.yaohoudy.com/smallpackage",
+  // profit:"https://www.yaohoudy.com/smallpackage",
   // profit: "http://47.95.196.91:8080/smallpackage",
-  // profit: "http://192.168.1.106/smallpackage",
+  profit: "http://192.168.1.106/smallpackage",
   staticProfit: "https://static.yaohoudy.com/yaohou_tool"
 }
