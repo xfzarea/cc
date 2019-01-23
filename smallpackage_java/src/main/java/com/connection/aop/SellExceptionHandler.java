02b14fafeb219c0944debf93ca4a93afe141b8b3
xfzarea <826806662@qq.com>
2019-01-23 18:09:17 +0800

    风控修改

--- smallpackage_java/src/main/java/com/connection/aop/SellExceptionHandler.java
+++ smallpackage_java/src/main/java/com/connection/aop/SellExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class SellExceptionHandler {
 	
-	   
+	   //如果单使用@ExceptionHandler，只能在当前Controller中处理异常。但当配合@ControllerAdvice一起使用的时候，就可以摆脱那个限制了。
 	    @ExceptionHandler(value = MyCustomException.class)
 	    public void handlerAuthorizeException(){
 	        
